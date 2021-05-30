9cb030e9f71f4529b7951cdcd078bcca36e3fb5a
EC2 Default User <ec2-user@ip-172-31-93-230.ec2.internal>
2021-05-30 12:02:51 +0000

    [update]3-7_画像投稿機能_attachmentメソッド追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_30_025045) do
+ActiveRecord::Schema.define(version: 2021_05_30_115241) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
