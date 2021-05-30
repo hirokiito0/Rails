c8fae3d95e47d7d3da4540893ba9ee5067deb1dc
EC2 Default User <ec2-user@ip-172-31-93-230.ec2.internal>
2021-05-30 12:07:56 +0000

    [update]3-7_画像投稿機能_Strong Parametersに追加

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -35,7 +35,7 @@ class TodolistsController < ApplicationController
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :ima)
   end
     
   
